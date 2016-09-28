436a650f8a8cac79f4aedc9ad0603bc42a99b0c8
TylerStein <tyler.stein1@yahoo.ca>
2016-09-28 11:20:56 -0400

    Chapter 2 Halfway
    Imported assets, created some prefabs

---
+++ CrazyMadOfficeDude/Assets/Editor/FBX_Import.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+public class FBX_Import : AssetPostprocessor {
+
+    public const float importScale = 25.0f;
+
+    void OnPreprocessModel()
+    {
+        ModelImporter importer = assetImporter as ModelImporter;
+        importer.globalScale = importScale;
+    }
+}
